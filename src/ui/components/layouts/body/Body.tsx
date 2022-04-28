d0ddc566a51c478a4bebd4f9e8480c34fc1c4c81
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-04-28 17:14:54 +0200

    added body layout

---
+++ src/ui/components/layouts/body/Body.tsx
@@ -0,0 +1,12 @@
+import styles from './Body.module.scss'
+
+const Body = () => (
+  <body className={styles.body}>
+    <div className={styles.content}>
+      <p>body</p>
+    </div>
+  </body>
+  
+);
+
+export default Body;
