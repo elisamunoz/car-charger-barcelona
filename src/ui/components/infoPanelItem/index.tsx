f70103252bd16a46a9cab48ea2523bbdbc8ae3d3
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2022-05-03 12:52:48 +0200

    info panel item

---
+++ src/ui/components/infoPanelItem/index.tsx
@@ -0,0 +1,3 @@
+import InfoPanelItem from './InfoPanelItem';
+
+export default InfoPanelItem;
--- src/ui/layouts/body/Body.tsx
+++ src/ui/layouts/body/Body.tsx
@@ -1,11 +1,15 @@
-import InfoPanel from '../../components/InfoPanel'
-
+import InfoPanel from '../../components/infoPanel'
 import styles from './Body.module.scss'
 
 const Body = () => (
   <div className={styles.body}>
-    <div className={styles.content}>
-      <InfoPanel />        
+    <div className={styles.sidePanel}>
+      <InfoPanel>
+        <InfoPanel.Item 
+          mainTitle='BAMSA Rambla de Catalunya - Endesa'
+          title='BAMSA - Rambla de Catalunya, Barcelona, Spain'
+          subtitle='Charging points' />
+      </InfoPanel>
     </div>
   </div>
 );
